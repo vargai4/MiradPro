43d2dfe732bbffcc833e3b8280dbc7291c1c5741
invabadilla <invabadilla@gmail.com>
2025-03-03 20:43:48 -0600

    Fix name

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SCQ7ZW6A.mjs";import"./chunk-BPZXAMS2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-X5VJV6RG.mjs";import"./chunk-BPZXAMS2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mirad' title`, () => {
+  it(`should have the 'MiradPro' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mirad');
+    expect(app.title).toEqual('MiradPro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mirad');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MiradPro');
   });
 });
